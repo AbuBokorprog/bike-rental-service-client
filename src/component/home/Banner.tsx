81fa5a1aa0c719b927e4f06c9afe70825cf6ff90
AbuBokorprog <abubokor1066@gmail.com>
2024-09-11 23:45:20 +0600

    Banner issue fixed.

--- src/component/home/Banner.tsx
+++ src/component/home/Banner.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import { Autoplay, Navigation } from "swiper/modules";
-import { Button } from "@mui/material";
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Autoplay, Navigation } from 'swiper/modules';
+import { Button } from '@mui/material';
 
 const Banner: React.FC = () => {
   return (
@@ -15,6 +15,30 @@ const Banner: React.FC = () => {
         modules={[Navigation, Autoplay]}
         className="mySwiper"
       >
+        <SwiperSlide className="relative w-full">
+          <img
+            src="/src/assets/images/banner.png"
+            alt="banner"
+            className="rounded-md"
+          />
+          <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2">
+            <Button variant="contained" color="primary">
+              Rent Bike
+            </Button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="relative w-full">
+          <img
+            src="/src/assets/images/banner-2.png"
+            alt="banner"
+            className="rounded-md"
+          />
+          <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2">
+            <Button variant="contained" color="primary">
+              Rent Bike
+            </Button>
+          </div>
+        </SwiperSlide>
         <SwiperSlide className="relative w-full">
           <img
             src="/src/assets/images/banner-3.png"
